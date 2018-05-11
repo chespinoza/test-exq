04189570a585fb7849aef961d2a3cc257d9eaf02
Christian Espinoza <chespinoza@gmail.com>
2018-05-11 01:53:31 +0100

    adds exq_ui

--- mix.exs
+++ mix.exs
@@ -14,7 +14,11 @@ defmodule TestExq.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [
+        :logger,
+        :exq,
+        :exq_ui
+      ]
     ]
   end
 
@@ -23,7 +27,8 @@ defmodule TestExq.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:exq, "~> 0.10.1"}
+      {:exq, "~> 0.10.1"},
+      {:exq_ui, "~> 0.9.0"}
     ]
   end
 end
